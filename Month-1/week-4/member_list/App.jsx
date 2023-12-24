7a9ec49ac56036ebabe529ac4f6a21c8764284d1
helinaz27 <helinazeleke670@gmail.com>
2023-12-24 19:47:01 +0300

    Added member_list

---
+++ Month-1/week-4/member_list/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import GroupMembersList from './memberlists'
+import blen from './Helina.jpg'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <img src={Helina} alt="Group" className='image' width="100px" height="100px"/>
+     <GroupMembersList members={['Biruk Mintesnot Mesfin', 'Bisrat Ambachew', 'Blien moges ','Elias Derbew','etsubtariku@gmail.com','Eyerusalem Ashagere Tsega','Eyob','Fikru Demlew ','Habtemariam Bereket','Hayat Hussen','Hayou Lemessa Ayana ','Helina zeleke']} />
+    </>
+  )
+}
+
+export default App
+
