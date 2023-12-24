e74b03518b636852e430e76b2fe239ad5fb0f6b6
helinaz27 <helinazeleke670@gmail.com>
2023-12-24 19:41:38 +0300

    Added BMI-Calculator

---
+++ Month-1/week-2/BMI calculator.js
@@ -0,0 +1,9 @@
+function calculateBMI() {
+     
+    var mass = prompt("Enter your mass in kilograms: ");
+    var height = prompt("Enter your height in meters: ");
+
+  console.log("Your BMI is:", mass / (height ** 2));
+  }
+calculateBMI();
+  
