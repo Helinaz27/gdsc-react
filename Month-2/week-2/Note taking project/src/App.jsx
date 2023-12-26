2c19db68d0d28dcf658b3d4f6e97b71bbf592a48
Helinaz27 <151021121+Helinaz27@users.noreply.github.com>
2023-12-26 23:11:28 +0300

    Added Note taking project

--- Month-2/week-2/Note taking project/src/App.jsx
+++ Month-2/week-2/Note taking project/src/App.jsx
@@ -2,58 +2,67 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-    const [notes, setNotes] = useState([]);
-    const [noteInput, setNoteInput] = useState('');
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState('');
+  const [editedNoteIndex, setEditedNoteIndex] = useState(null);
 
-    const addNote = () => {
-        if (noteInput.trim() !== '') {
-            setNotes([...notes, noteInput]);
-            setNoteInput('');
-        }
-    };
+  const addNote = () => {
+    if (newNote.trim() !== '' && newNote.length < 500) {
+      if (editedNoteIndex !== null) {
+       
+        const updatedNotes = [...notes];
+        updatedNotes[editedNoteIndex] = newNote;
+        setNotes(updatedNotes);
+        setEditedNoteIndex(null);
+      } else {
+      
+        setNotes([...notes, newNote]);
+      }
+      setNewNote('');
+    }
+  };
 
-    const editNote = (index) => {
-        const newText = prompt('Edit the note:', notes[index]);
-        if (newText !== null) {
-            const newNotes = [...notes];
-            newNotes[index] = newText;
-            setNotes(newNotes);
-        }
-    };
+  const editNote = (index) => {
+    setNewNote(notes[index]);
+    setEditedNoteIndex(index);
+  };
 
-    const deleteNote = (index) => {
-        if (window.confirm('Are you sure you want to delete this note?')) {
-            const newNotes = [...notes];
-            newNotes.splice(index, 1);
-            setNotes(newNotes);
-        }
-    };
+  const deleteNote = (index) => {
+    const deletedNote = notes[index];
+    const updatedNotes = [...notes];
+    updatedNotes.splice(index, 1);
+    setNotes(updatedNotes);
 
-    return (
-        <div id="app">
-            <h1>My Notes</h1>
-            <div>
-                <input
-                    type="text"
-                    placeholder="Add a new note"
-                    value={noteInput}
-                    onChange={(e) => setNoteInput(e.target.value)}
-                />
-                <button onClick={addNote}>Add Note</button>
-            </div>
-            <ul>
-                {notes.map((note, index) => (
-                    <li key={index}>
-                        {note}
-                        <button onClick={() => editNote(index)}>Edit</button>
-                        <button onClick={() => deleteNote(index)}>Delete</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+    console.log(`Deleted: ${deletedNote}`);
+  };
+
+  return (
+    <div className="App">
+      <h1>My Notes</h1>
+      <div>
+        <textarea
+          value={newNote}
+          onChange={(e) => setNewNote(e.target.value)}
+          placeholder="Enter your note"
+          rows="4"
+          cols="50"
+        ></textarea>
+        <button onClick={addNote}>{editedNoteIndex !== null ? 'Update Note' : 'Add Note'}</button>
+      </div>
+      <ul>
+        {notes.map((note, index) => (
+          <li key={index} className={editedNoteIndex === index ? 'edited' : ''}>
+            <p style={{ textDecoration: editedNoteIndex === index ? 'line-through' : 'none' }}>{note}</p>
+            <button onClick={() => editNote(index)}>Edit</button>
+            <button onClick={() => deleteNote(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
+
 
 
